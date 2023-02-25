20bc7268527141662f674dce347c13079511c143
LourenceQ <lawrenceqf@gmail.com>
2023-02-25 11:38:03 -0300

    Linq query to get customer's name and use XElement

--- LinqInAction.Main/Program.cs
+++ LinqInAction.Main/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+namespace LinqInAction.Main;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var contacts = from customter in db.Customers
+            where customter.Name.StartsWith("A") 
+            && customter.ORders.COunt > 0
+            orderby customter.Name
+            select new{customer.Name, customer.Phone};
+
+        var xml = 
+        new XElement("contacts",
+        from contact in contacts
+        select new XElement("contact",
+            new XAttribute("name", contact.Name),
+            new XAttribute("phone", contact.Phone)
+            )
+        );
+    }
+}
