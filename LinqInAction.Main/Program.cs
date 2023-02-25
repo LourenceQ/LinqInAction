fab8fe7d313414a596c8905969324d31b6cb3f88
LourenceQ <lawrenceqf@gmail.com>
2023-02-25 19:16:13 -0300

    Fazendo engenharia reversa de banco de dados Northwind

--- LinqInAction.Main/Program.cs
+++ LinqInAction.Main/Program.cs
@@ -6,19 +6,19 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var contacts = from customter in db.Customers
-            where customter.Name.StartsWith("A") 
-            && customter.ORders.COunt > 0
-            orderby customter.Name
-            select new{customer.Name, customer.Phone};
+        // var contacts = from customter in db.Customers
+        //     where customter.Name.StartsWith("A") 
+        //     && customter.Orders.Count > 0
+        //     orderby customter.Name
+        //     select new{customer.Name, customer.Phone};
 
-        var xml = 
-        new XElement("contacts",
-        from contact in contacts
-        select new XElement("contact",
-            new XAttribute("name", contact.Name),
-            new XAttribute("phone", contact.Phone)
-            )
-        );
+        // var xml = 
+        // new XElement("contacts",
+        // from contact in contacts
+        // select new XElement("contact",
+        //     new XAttribute("name", contact.Name),
+        //     new XAttribute("phone", contact.Phone)
+        //     )
+        // );
     }
 }
