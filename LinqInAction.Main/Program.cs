1a1682bb44208f4b18ee7066a1968031eb2bac89
LourenceQ <lawrenceqf@gmail.com>
2023-02-27 22:17:30 -0300

    Subwuery com ordenação em Linq

--- LinqInAction.Main/Program.cs
+++ LinqInAction.Main/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqInAction.Main;
+﻿using System.Xml.Linq;
+
+namespace LinqInAction.Main;
 
 public class Program
 {
@@ -6,24 +8,50 @@ public class Program
     {
         var db = new NorthwindContext();
 
-         var contacts = from customer in db.Customers
-             where customer.CompanyName.StartsWith("A") 
-             && customer.Orders.Count > 0
-             orderby customer.CompanyName
-                select new{ customer.CompanyName, customer.Phone};
+        var contacts = from customer in db.Customers
+                       where customer.CompanyName.StartsWith("A")
+                       && customer.Orders.Count > 0
+                       orderby customer.CompanyName
+                       select new { customer.CompanyName, customer.Phone };
 
         foreach (var item in contacts)
         {
             Console.WriteLine(item);
         }
 
-        // var xml = 
-        // new XElement("contacts",
-        // from contact in contacts
-        // select new XElement("contact",
-        //     new XAttribute("name", contact.Name),
-        //     new XAttribute("phone", contact.Phone)
-        //     )
-        // );
+        /*var xml =
+        new XElement("contacts",
+        from contact in contacts
+        select new XElement("contact",
+            new XAttribute("name", contact.CompanyName),
+            new XAttribute("phone", contact.Phone)
+            )
+        );*/
+
+        // Hello LINQ to objects
+
+        string[] words = { "hello", "wonderful", "linq", "beautiful", "world" };
+
+        var shortWords = from word in words
+                         where word.Length <= 5
+                         select word;
+
+        foreach (var item in shortWords)
+        {
+            Console.WriteLine(item);
+        }
+
+        var groups = from word in shortWords
+                     orderby word ascending
+                     group word by word.Length into lengthGroups
+                     orderby lengthGroups.Key descending
+                     select new { Length = lengthGroups.Key, Words = lengthGroups };
+
+        foreach (var item in groups)
+        {
+            Console.WriteLine("Words of length " + item.Length);
+            foreach (string w in item.Words)
+                Console.WriteLine(" " + w);
+        }
     }
 }
