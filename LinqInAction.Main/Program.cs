d267978b068e51a4a00a796366d45188d53e99fb
LourenceQ <lawrenceqf@gmail.com>
2023-04-24 22:14:03 -0300

    Implementind a extension method

--- LinqInAction.Main/Program.cs
+++ LinqInAction.Main/Program.cs
@@ -1,28 +1,51 @@
-﻿namespace LinqInAction.Main;
+﻿using System.Xml.Linq;
+using static System.Console;
+
+namespace LinqInAction.Main;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        #region LAMBDA TESTS
         ReturnCustomers();
         var numbers = new[] { 1, 2, 3, 4, 5, 6, 222, 333 };
+        var wordsTest = new[] { "aaa", "bbb", "cdfe" };
 
         Console.WriteLine($"Is any > 100 ?: {IsAny(numbers, IsAnyLargerThan100)}");
 
         Console.WriteLine($"Is any even ?: {IsAny(numbers, IsAnyEven)}");
 
+        Console.WriteLine(IsAny(wordsTest, word => word.Length == 4));
+        #endregion
+
+        #region EXTENSIONS TESTS
+        var multilineString = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of 
+                                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+                                publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+
+        var countOfLines = multilineString.GetCountOfLines();
+        WriteLine();
+        WriteLine("EXTENSIONS TESTS");
+        WriteLine($"Lines: {countOfLines}");
+        WriteLine();
+        #endregion
 
         #region XML
-
-        var xml =
+        /*var xml =
         new XElement("contacts",
         from contact in contacts
         select new XElement("contact",
             new XAttribute("name", contact.CompanyName),
             new XAttribute("phone", contact.Phone)
             )
-        );
+        );*/
         #endregion
+
         #region SIMPLE QUERY IN ARRAY
         // Hello LINQ to objects
 
@@ -76,7 +99,7 @@ public class Program
 
     public static bool IsAnyEven(int number) { return number % 2 == 0; }
 
-    public static bool IsAny(int[] numbers, Func<int, bool> predicate)
+    public static bool IsAny<T>(IEnumerable<T> numbers, Func<T, bool> predicate)
     {
         foreach (var number in numbers)
             if (predicate(number)) return true;
@@ -104,3 +127,13 @@ public class Program
     }
     #endregion
 }
+
+#region Extension Methods
+public static class StringExtensions
+{
+    public static int GetCountOfLines(this string input)
+    {
+        return input.Split("\n").Length;
+    }
+}
+#endregion
